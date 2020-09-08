3c57cd088dd4728f76fc092b5f6ec9975935fc12
Dmytro <autumnmeds@gmail.com>
2020-09-08 10:27:06 +0300

    Add solution for task 2 BBC1.Part1

--- src/main/java/pages/NewsPage.java
+++ src/main/java/pages/NewsPage.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class NewsPage extends BasePage {
 
     @FindBy(xpath = "//span[contains(text(),'BBC News')]")
     private WebElement expectedArticle;
+
+    @FindBy(xpath = "//ul[@class='gs-o-list-ui--top-no-border nw-c-nav__wide-sections']/li/a/span[1]")
+    private List<WebElement> actualListOfSecondaryArticleTitles;
 
     public NewsPage(WebDriver driver) {
         super(driver);
@@ -20,6 +26,21 @@ public class NewsPage extends BasePage {
 
     public String getExpectedTextOfTheArticle() {
         return getExpectedArticle().getText();
+    }
+
+    public List<WebElement> getActualListOfSecondaryArticleTitles() {
+        return actualListOfSecondaryArticleTitles;
+    }
+
+    public ArrayList<String> stringArrayListOfActualSecondaryArticleTitles() {
+        ArrayList<String> actualList = new ArrayList<>();
+        for (WebElement tab : getActualListOfSecondaryArticleTitles()) {
+            String tabOfList = tab.getText();
+            if (tabOfList.length() != 0) {
+                actualList.add(tabOfList);
+            }
+        }
+        return actualList;
     }
 }
 
