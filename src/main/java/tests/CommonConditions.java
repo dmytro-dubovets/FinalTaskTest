8589361ea739eb65a397582e0812242fee0945df
dmytro-dubovets <autumnmeds@gmail.com>
2020-09-12 21:30:39 +0300

    Previous code was not saved

--- src/main/java/tests/CommonConditions.java
+++ src/main/java/tests/CommonConditions.java
@@ -1,22 +1,44 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 
 public class CommonConditions {
 
     protected WebDriver driver;
     protected String url = "https://www.bbc.com/";
 
+    protected By windowOff = By.xpath("//button[@class='sign_in-exit']");
 
     @BeforeSuite
     public void profileSetUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+
+    @BeforeMethod
+    public void switchToDefaultContent() {
+        driver.switchTo().defaultContent();
+    }
+
+    @AfterMethod
+    public void writeTextMsgIfWindowIsDisplayed() {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 1);
+            WebElement button = wait.until(ExpectedConditions.presenceOfElementLocated(windowOff));
+            if (button.getAttribute("data-bbc-title").contains("dismiss")) {
+                button.click();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @BeforeTest
